a2c97863047a84e6cd8c7e0bd75aba4cfce16270
pteras <panu.teras@edu.turkuamk.fi>
2022-11-03 11:11:01 +0200

    Updated scraper.py
    Made API work

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 gmaps = googlemaps.Client(key="AIzaSyB4LyVDfTiPPS6cLQvRGLJMSTEE0Dp3rLs")
 
-# Testing geocode
+# Testing geocode #
 address_geocode = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
 print(address_geocode)
 

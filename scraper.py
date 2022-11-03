8e1dca846b560166459faefef1f34dfbbd4536ec
pteras <panu.teras@edu.turkuamk.fi>
2022-11-03 11:09:14 +0200

    Fixed API key
    Made it work

--- scraper.py
+++ scraper.py
@@ -1,17 +1,9 @@
 import googlemaps
 from datetime import datetime
 
-gmaps = googlemaps.Client(key='AIzaSyB4LyVDfTiPPS6cLQvRGLJMSTEE0Dp3rLs')
+gmaps = googlemaps.Client(key="AIzaSyB4LyVDfTiPPS6cLQvRGLJMSTEE0Dp3rLs")
 
-address = input('Anna osoitteesi:  \n')
-# Address geocoding = osoite -> long ja lat
-address_geocode = gmaps.geocode(input)
+# Testing geocode
+address_geocode = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
 print(address_geocode)
-
-# kysy osoite
-# kysy etäisyys 
-# mitä haluaa hakea? esim kahvila, pizza, burger
-# laske mikä yritys lähin 
-# rajaa vastaukset top 10 (sort by lähin ja kerro etäisyys)
-# profit 
 

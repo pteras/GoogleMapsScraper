120a197ac53a76d039e48148bd0ec719f17fb49e
pteras <panu.teras@edu.turkuamk.fi>
2022-11-04 14:21:43 +0200

    gets address long lat values

--- scraper.py
+++ scraper.py
@@ -1,20 +1,30 @@
 import googlemaps
 from math import radians, cos, sin, asin, sqrt
 from datetime import datetime
+import json
+import os
 
 gmaps = googlemaps.Client(key="AIzaSyB4LyVDfTiPPS6cLQvRGLJMSTEE0Dp3rLs")
 
 # Testing geocode
-address_geocode = gmaps.geocode('Ruukinrinne 4, Turku, Finland')
-#print(address_geocode)
+#address = 'Ruukinrinne 4 Turku'
+address = input('Type in your address: \n')
+business = input('What kind of business/restaurant do you want to search: \n')
+meters = int(input('Search radius in meters: \n'))
 
+address_longlat = gmaps.geocode(address)[0]
+#print(address_longlat)
+#address_longlat.keys()
+lat1 = address_longlat['geometry']['location']['lat']
+lon1 = address_longlat['geometry']['location']['lng']
 
-paikka = gmaps.find_place("restaurant", "textquery",location_bias="circle:10@60.45519009999999,22.3045487")
-print(paikka)
-print(gmaps.reverse_geocode(paikka["candidates"][0]["place_id"]))
+#print(lat1)
+#print(lon1)
 
-# Get location 
-
+address_longlat = lat1,lon1
+# !!!
+places = gmaps.places_nearby(location=address_longlat,radius=meters, type=business)
+print(json.dumps(places, indent=2))
 
 # Calculate Distance Between Two Points on Earth
 def distance(lat1, lat2, lon1, lon2):
@@ -38,14 +48,20 @@ def distance(lat1, lat2, lon1, lon2):
       
     # calculate the result
     return(c * r)
-     
-     
-# driver code
-lat1 = 60.45519009999999
-lon1 = 22.3045487
+       
+# driver code for distance
+# need to get places longs and lats !!! 
+
 lat2 = 60.45715565610971
 lon2 =  22.29241985762579
-#print(distance(lat1, lat2, lon1, lon2), "km")
-# jos alle km ni muuta metreiksi
+
+# need to round results
+if distance(lat1, lat2, lon1, lon2) >= 1:
+    print(distance(lat1, lat2, lon1, lon2), "km")
+else: 
+    print(distance(lat1, lat2, lon1, lon2)*1000, "m")
 
 
+#paikka = gmaps.find_place("restaurant", "textquery",location_bias="circle:10@60.45519009999999,22.3045487")
+#print(paikka)
+#print(gmaps.reverse_geocode(paikka["candidates"][0]["place_id"]))
